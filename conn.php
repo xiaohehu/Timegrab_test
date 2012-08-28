13ff9cd7f48a434ea11dbf5d01b714f2fdb0f0a2
xiaohehu <huxiaohe113@gmail.com>
2012-08-28 20:15:18 -0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -1,12 +1,12 @@
 <?php
 
-$con = mysql_connect("localhost","root","123456");
+$con = mysql_connect("instance25358.db.xeround.com:8734","xiaohehu","19881103");
 // if connection to the database fails an error is thrown
 if (!$con){
 die('Could not connect: ' . mysql_error());
 }
 // selecting the database for the connection created
-mysql_select_db("timegrab_test", $con);
+mysql_select_db("Timegrab_test", $con);
 // storing the query in a variable called $query
 
 ?>
